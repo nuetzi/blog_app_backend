d65266a3b8ce2b3a31bd161b8a1f73829568b2c2
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-04-23 01:22:58 -0400

    Adding new user schema and route

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const usersSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    username: {type: String, required: true},
     password: {type: String, required: true}
 });
 
